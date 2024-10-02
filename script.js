a734ca5d3bf530e4d8de6a402dab3e4a93037d7e
Abdulrofihi Musa <m.abdulrofihi@gmail.com>
2024-10-02 18:13:24 +0100

    Add getComputerChoice() to the script

--- script.js
+++ script.js
@@ -1,1 +1,5 @@
-console.log("Hello, world!");
+function getComputerChoice() {
+    let values = ["rock", "paper", "scissors"];
+    let randIndex = Math.floor(Math.random() * 3);
+    return values[randIndex];
+}
