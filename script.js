61f5445dd2f954af8237ca8ddac024235bd87b74
Abdulrofihi Musa <m.abdulrofihi@gmail.com>
2024-10-02 21:14:09 +0100

    Add getHumanChoice()

--- script.js
+++ script.js
@@ -3,3 +3,18 @@ function getComputerChoice() {
     let randIndex = Math.floor(Math.random() * 3);
     return values[randIndex];
 }
+
+function getHumanChoice() {
+    let values = ['rock', 'paper', 'scissors'];
+    let choice = prompt("Rock/Paper/Scissors.. what do you choose?");
+    for (let i = 0; i < values.length; i++) {
+        if (values[i] === choice) {
+            return choice;
+        }
+    }
+
+    return "Please, choose either of these items - Rock/Paper/Scissors.";
+}
+
+console.log(getComputerChoice());
+console.log(getHumanChoice());
